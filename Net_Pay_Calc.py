10e3bca04aa931befb4eea815a89b9551cf8d0b4
SchnitzelB <es971120@icloud.com>
2021-04-13 09:29:26 -0500

    Adjusted comments

--- Net_Pay_Calc.py
+++ Net_Pay_Calc.py
@@ -1,8 +1,5 @@
-#Calculate Net Pay for an employee working one week
-# and getting paid by the hour with the assumptions below
-
-## This should provide the accurate output based on any number of
-## hours of input and any wage per hour.
+# Calculate Net Pay for an employee working one week and getting paid by the hour with the assumptions below
+# This should provide the accurate output based on any number of hours of input and any wage per hour.
 
 # All statements will run within a while loop
 moreLoops = True  # A variable of type boolean will always store a value of True so the program can start
@@ -14,20 +11,10 @@ while moreLoops == True:
   hourlyWage = float(input("Insert the employee's hourly wage --> ")) # Create a variable of float and store the hourly wage of the employee
   hoursWorked = float(input("Insert the employee's total hours worked --> ")) # Create a variable of type float and store the total hours the employee has worked
 
-  ## FUNCTIONS
-  ## grossPay(double hourlyWage, double hoursWorked)
-  ##   double otHours;
-  ##   double otWage = hourlyWage + (hourlyWage / 2)
-  ##   if(hoursWorked > 40) 
-  ##        otHours = hoursWorked - 40;
-  ##        return (double) (hourlyWage * hoursWorked) + (otWage * otHours);
-  ##   elif(hoursWorked <= 40 && hoursWorked > 0)
-  ##        return hourlyWage * hoursWorked; 
-
   def grossPayCalc(hourlyWage,hoursWorked):         # The function takes in two (2) parameters of type float, hourlyWage and hoursWorked
     otWage = (float(hourlyWage + (hourlyWage / 2))) # The variable, otWage of type float, is declared and assigned a value of "1.5x hourlyWage", i.e. "Time and a half".
     if hoursWorked > 40:                            # If overtime hours are detected,
-      otHours = (float(hoursWorked - 40))           # Variable, otHours of type float, is declared and is assigned a value of Overtime Hours only. i.e. if 45 hours are worked, the value of 5 will be assigned to otHours.
+      otHours = (float(hoursWorked - 40))           # Variable, otHours of type float, is declared and is assigned a value of Overtime Hours only. i.e. if 45 hours are worked, otHours will equal 5.
       print("Overtime hours detected. ", otHours, " overtime hours worked.") # This print statement will only print out the variable otHours
       return (hourlyWage * hoursWorked) + (otWage * otHours) # The regular wage of regular hours worked, plus the wage of overtime hours worked will be returned when grossPayCalc() is called.
     elif hoursWorked <= 40 and hoursWorked > 0: # If the total amount of hours worked is less than or equal to 40 and greater than zero, return the regular wage times the hours worked.
