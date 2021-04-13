3fa97f46e8295a625600da9d9d31e8a89966a352
SchnitzelB <es971120@icloud.com>
2021-04-13 08:19:26 -0500

    Output name, gross pay and net pay

--- Net_Pay_Calc.py
+++ Net_Pay_Calc.py
@@ -4,42 +4,44 @@
 ## This should provide the accurate output based on any number of
 ## hours of input and any wage per hour.
 
+moreLoops = True
 
+while moreLoops == True:
 
 ## VARIABLES
-employeeName = input("Insert the employee's Full Name --> ")
+  employeeName = input("Insert the employee's Full Name --> ")
+  employeeList = []
 
-hourlyWage = float(input("Insert the employee's hourly wage --> "))
-hoursWorked = float(input("Insert the employee's total hours worked --> "))
+  hourlyWage = float(input("Insert the employee's hourly wage --> "))
+  hoursWorked = float(input("Insert the employee's total hours worked --> "))
 
 
-## FUNCTIONS
-## grossPay(double hourlyWage, double hoursWorked)
-##   double otHours;
-##   double otWage = hourlyWage + (hourlyWage / 2)
-##   if(hoursWorked > 40) 
-##        otHours = hoursWorked - 40;
-##        return (double) (hourlyWage * hoursWorked) + (otWage * otHours);
-##   elif(hoursWorked <= 40 && hoursWorked > 0)
-##        return hourlyWage * hoursWorked; 
+  ## FUNCTIONS
+  ## grossPay(double hourlyWage, double hoursWorked)
+  ##   double otHours;
+  ##   double otWage = hourlyWage + (hourlyWage / 2)
+  ##   if(hoursWorked > 40) 
+  ##        otHours = hoursWorked - 40;
+  ##        return (double) (hourlyWage * hoursWorked) + (otWage * otHours);
+  ##   elif(hoursWorked <= 40 && hoursWorked > 0)
+  ##        return hourlyWage * hoursWorked; 
 
-def grossPayCalc(hourlyWage,hoursWorked):         # The function must take in two (2) parameters of type float, hourlyWage and hoursWorked
-  otWage = (float(hourlyWage + (hourlyWage / 2))) # The variable, otWage of type float, is declared and assigned a value of "1.5x hourlyWage", i.e. "Time and a half".
-  if hoursWorked > 40:                            # If overtime hours are detected,
-    otHours = (float(hoursWorked - 40))           # Variable, otHours of type float, is declared and is assigned a value of Overtime Hours only. i.e. if 45 hours are worked, the value of 5 will be assigned to otHours.
-    print("Overtime hours detected. ", otHours, " overtime hours worked.") #This print function will only print out the amount of overtime hours worked.
-    return (hourlyWage * hoursWorked) + (otWage * otHours) # When grossPayCalc() is called, the function will return a variable of type float. In this return statement, the employees total compensation (Normal work pay for 40 hours, plus, time and a half for every hour worked) is returned.
-  elif hoursWorked <= 40 and hoursWorked > 0:     # If total hours worked is less than, or equal to, 40 AND greater than zero,
-    return hourlyWage * hoursWorked               # Time and a half is not calculated, therefore, returning the normal compensation amount.
+  def grossPayCalc(hourlyWage,hoursWorked):         # The function takes in two (2) parameters of type float, hourlyWage and hoursWorked
+    otWage = (float(hourlyWage + (hourlyWage / 2))) # The variable, otWage of type float, is declared and assigned a value of "1.5x hourlyWage", i.e. "Time and a half".
+    if hoursWorked > 40:                            # If overtime hours are detected,
+      otHours = (float(hoursWorked - 40))           # Variable, otHours of type float, is declared and is assigned a value of Overtime Hours only. i.e. if 45 hours are worked, the value of 5 will be assigned to otHours.
+      print("Overtime hours detected. ", otHours, " overtime hours worked.")
+      return (hourlyWage * hoursWorked) + (otWage * otHours)
+    elif hoursWorked <= 40 and hoursWorked > 0:
+      return hourlyWage * hoursWorked
 
-grossPay = grossPayCalc(hourlyWage,hoursWorked)   # The variable, grossPay of type float, is declared and assigned the value that is returned from grossPayCalc()
-netPay = float(grossPay - (grossPay * 0.1))       # Variable, netPay of type float, is declared and assigned the value of (grossPay - 10%)
+  grossPay = grossPayCalc(hourlyWage,hoursWorked)
+  netPay = float(grossPay - (grossPay * 0.1))
 
-print("Employee Name: " + employeeName + 
-      "\nGross Pay: $" + str(grossPay) + 
-      "\nNet Pay: $" + str(netPay)) # Add the employee information on the next line, without adding a new print() function
-# The print function is called to output the employee's name, gross pay and net pay, while type casting all variables to Strings.
+  #for i = 0; moreLoops == True; i + 1:
 
-## Taxes on wages
-# netPay(double grossPay)
-#   return (double) grossPay - (grossPay * 0.1)
+  #employeeList.append()
+
+  print("Employee Name: " + str(employeeName) + 
+        "\n Gross Pay: $" + str(grossPay) + 
+        "\n Net Pay: $" + str(netPay)) # Add the employee information on the next line, without adding a new print() function
