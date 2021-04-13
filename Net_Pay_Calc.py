4ef2303ec48b41fe0274f4e055026b817ebaf750
SchnitzelB <es971120@icloud.com>
2021-04-13 14:51:15 -0500

    Integrate array to store employee payroll info

--- Net_Pay_Calc.py
+++ Net_Pay_Calc.py
@@ -1,54 +1,58 @@
-#Calculate Net Pay for an employee working one week
-# and getting paid by the hour with the assumptions below
+# Calculate Net Pay for an employee working one week and getting paid by the hour with the assumptions below
+# This should provide the accurate output based on any number of hours of input and any wage per hour.
 
-## This should provide the accurate output based on any number of
-## hours of input and any wage per hour.
-
-moreLoops = True
+# All statements will run within a while loop
+moreLoops = True  # A variable of type boolean will always store a value of True so the program can start
+payrollArray = []
 
 while moreLoops == True:
 
 ## VARIABLES
-  employeeName = input("Insert the employee's Full Name --> ")
-
-  hourlyWage = float(input("Insert the employee's hourly wage --> "))
-  hoursWorked = float(input("Insert the employee's total hours worked --> "))
-
-
-  ## FUNCTIONS
-  ## grossPay(double hourlyWage, double hoursWorked)
-  ##   double otHours;
-  ##   double otWage = hourlyWage + (hourlyWage / 2)
-  ##   if(hoursWorked > 40) 
-  ##        otHours = hoursWorked - 40;
-  ##        return (double) (hourlyWage * hoursWorked) + (otWage * otHours);
-  ##   elif(hoursWorked <= 40 && hoursWorked > 0)
-  ##        return hourlyWage * hoursWorked; 
+  employeeName = input("Insert the employee's Full Name --> ") # Create a variable of type String and store the employee's name
+  hourlyWage = float(input("Insert the employee's hourly wage --> ")) # Create a variable of float and store the hourly wage of the employee
+  hoursWorked = float(input("Insert the employee's total hours worked --> ")) # Create a variable of type float and store the total hours the employee has worked
 
   def grossPayCalc(hourlyWage,hoursWorked):         # The function takes in two (2) parameters of type float, hourlyWage and hoursWorked
     otWage = (float(hourlyWage + (hourlyWage / 2))) # The variable, otWage of type float, is declared and assigned a value of "1.5x hourlyWage", i.e. "Time and a half".
     if hoursWorked > 40:                            # If overtime hours are detected,
-      otHours = (float(hoursWorked - 40))           # Variable, otHours of type float, is declared and is assigned a value of Overtime Hours only. i.e. if 45 hours are worked, the value of 5 will be assigned to otHours.
-      print("Overtime hours detected. ", otHours, " overtime hours worked.")
-      return (hourlyWage * hoursWorked) + (otWage * otHours)
-    elif hoursWorked <= 40 and hoursWorked > 0:
+      otHours = (float(hoursWorked - 40))           # Variable, otHours of type float, is declared and is assigned a value of Overtime Hours only. i.e. if 45 hours are worked, otHours will equal 5.
+      print("Overtime hours detected. ", otHours, " overtime hours worked.") # This print statement will only print out the variable otHours
+      return (hourlyWage * hoursWorked) + (otWage * otHours) # The regular wage of regular hours worked, plus the wage of overtime hours worked will be returned when grossPayCalc() is called.
+    elif hoursWorked <= 40 and hoursWorked > 0: # If the total amount of hours worked is less than or equal to 40 and greater than zero, return the regular wage times the hours worked.
       return hourlyWage * hoursWorked
 
-  grossPay = grossPayCalc(hourlyWage,hoursWorked)
-  netPay = float(grossPay - (grossPay * 0.1))
+  grossPay = grossPayCalc(hourlyWage,hoursWorked) # Create a variable of type float to calculate gross pay
+  incomeTaxes = grossPay * 0.1                    # Create a variable of type float to calculate taxes paid
+  netPay = float(grossPay - incomeTaxes)          # Create a variable of type float to calculate net pay
 
-  print("Employee Name: " + str(employeeName) + 
+  print("\nEmployee Name: " + str(employeeName) + 
+        "\n Hourly Wage: " + str(hourlyWage) + 
+        "\n Total Hours Worked: " + str(hoursWorked) +
         "\n Gross Pay: $" + str(grossPay) + 
-        "\n Net Pay: $" + str(netPay)) # Add the employee information on the next line, without adding a new print() function
+        "\n Taxes Paid: $" + str(incomeTaxes) +
+        "\n Net Pay: $" + str(netPay))
+  # Print all requirements (Employee Name, Hours Worked, Hourly Wage, Gross Pay, Taxes Paid and Net Pay)
 
-  ## Taxes on wages
-  # netPay(double grossPay)
-  #   return (double) grossPay - (grossPay * 0.1)
+  # Create an Array to hold individual paychecks for employees
+    # The first row will contain employee names
+    # The second column will contain the net pay to the corresponding employees
 
-  addEmployee = str(input("Add another employee? \n [Y]/[N]"))
+    # for i = 0; moreLoops == False; i++:
+      # arrayName.insert(i, employeeName + " " + netPay)
+      # print(arrayName[i])
+
+  payrollArray.append(str("Pay to the order of " + employeeName + ":    $" + str(netPay)))
+
+  addEmployee = str(input("Add another employee? \n [Y]/[N] --> "))
+  # After one employee's wages breakdown is output, the user will be asked if they would like to continue with another employee or if they are finished.
 
   if addEmployee == "y" or addEmployee == "Y":
     moreLoops = True
+    # If the user chooses to continue, the program will start from the beginning
   elif addEmployee == "n" or addEmployee == "N":
     print("Senpai, Shitsureishimasu! Matashita!")
     moreLoops = False
+    # If the user chooses not to continue, the while loop will no longer loop. 
+
+for i in payrollArray:
+  print(i)
